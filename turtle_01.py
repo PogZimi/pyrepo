2ebabe18a64fc8675defa6a020bac43822d05b17
Claxxy <105559019+PogZimi@users.noreply.github.com>
2022-12-31 23:02:37 +0530

    Create turtle_01.py

---
+++ turtle_01.py
@@ -0,0 +1,20 @@
+from turtle import * 
+
+color('yellow', 'dark grey')
+begin_fill()
+
+forward(250)
+backward(500)
+left(90)
+forward(330)
+right(123.5)
+forward(599.082632 + 0.89)
+
+end_fill()
+done()
+
+'''
+
+BASTARD I DIDNT STEAL THE CODE 
+
+'''
