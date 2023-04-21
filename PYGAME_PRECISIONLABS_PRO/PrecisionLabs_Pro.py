f3a18d960f3257da9d81954e1f12cb1253fa1fff
WACLOT <105559019+PogZimi@users.noreply.github.com>
2023-04-21 19:53:08 +0530

    Update PrecisionLabs_Pro.py

--- PYGAME_PRECISIONLABS_PRO/PrecisionLabs_Pro.py
+++ PYGAME_PRECISIONLABS_PRO/PrecisionLabs_Pro.py
@@ -3,7 +3,7 @@ MIT License
 
 PogZimi , dealtwixyn@gmail.com
 
-Copyright (c) [Enter the year(s) of copyright]
+Copyright (c) 2023
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
