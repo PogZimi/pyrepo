6a954e452481a22db5eac017e53f7f888682791d
WACLOT <105559019+PogZimi@users.noreply.github.com>
2023-04-22 12:16:21 +0530

    Update Loading_screen.py

--- Loading_screen.py
+++ Loading_screen.py
@@ -2,15 +2,13 @@ import pygame
 import random 
 import time 
 
-# Change width , height according to your wish 
-
 width = 1200
 height = 900
 
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('LOADING_SCREEN')
-BOOL_VAL = None 
-WHITE = (255,255,255)
+BOOL_VAL = None
+DELAY = 1 
 
 left_small = pygame.Rect(width//2 - 200, width//2 + 65, 2, 25)
 right_small = pygame.Rect(width//2 + 200, width//2 + 65, 2 , 25)
@@ -21,18 +19,18 @@ lower_big = pygame.Rect(left_small.x , left_small.y + left_small.height, (right_
 WHITE_REC_LOADER = pygame.Rect(left_small.x + left_small.width , left_small.y , 30, left_small.height+1)
 
 def increment():
-    random_num = random.randint(32, 46)
-    time.sleep(1)
+    random_num = random.randint(32, 44)
+    time.sleep(DELAY)
     WHITE_REC_LOADER.width = WHITE_REC_LOADER.width + random_num
 
 class LOADING_SCREEN:
     def update_screen(self):
         window.fill((0,0,0))
-        pygame.draw.rect(window, WHITE, left_small)
-        pygame.draw.rect(window, WHITE, right_small)
-        pygame.draw.rect(window, WHITE, upper_big)
-        pygame.draw.rect(window, WHITE, lower_big)
-        pygame.draw.rect(window, WHITE, WHITE_REC_LOADER)
+        pygame.draw.rect(window, (255,255,255), left_small)
+        pygame.draw.rect(window, (255,255,255), right_small)
+        pygame.draw.rect(window, (255,255,255), upper_big)
+        pygame.draw.rect(window, (255,255,255), lower_big)
+        pygame.draw.rect(window, (255,255,255), WHITE_REC_LOADER)
         pygame.display.update()
       
 o = LOADING_SCREEN()
@@ -43,7 +41,8 @@ while run:
         if event.type == pygame.QUIT:
             run = False 
     increment()
-    var = upper_big.width - left_small.width - 25
+    var = upper_big.width - left_small.width - (left_small.h+4)
+    # WHITE_REC_LOADER.width == var or WHITE_REC_LOADER.width < var and WHITE_REC_LOADER.width > var-6 
     if(WHITE_REC_LOADER.width > var):
             run = False
     o.update_screen()
